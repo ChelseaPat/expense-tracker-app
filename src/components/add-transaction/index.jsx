28be2e1b71cc49bcf34ee4ef4eb9247516c3c1f4
Chelsea Patterson <chelseapat77@gmail.com>
2024-03-25 21:22:32 +1300

    Create transaction chart

--- src/components/add-transaction/index.jsx
+++ src/components/add-transaction/index.jsx
@@ -3,11 +3,27 @@ import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
 import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
 import { Radio, RadioGroup } from "@chakra-ui/radio";
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
 
 export default function TransactionForm({onClose, isOpen}) {
+    const { formData, setFormData, value, setValue, handleFormSubmit } = useContext(GlobalContext);
+
+    function handleFormChange(event) {
+        setFormData({
+            ...formData,
+            [event.target.name] : event.target.value
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        handleFormSubmit(formData);
+    }
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>Add New Transaction</ModalHeader>
@@ -19,6 +35,7 @@ export default function TransactionForm({onClose, isOpen}) {
                                 placeholder="Enter Transaction description"
                                 name="description"
                                 type="text"
+                                onChange={handleFormChange}
                             />
                         </FormControl>
                         <FormControl>
@@ -27,16 +44,35 @@ export default function TransactionForm({onClose, isOpen}) {
                                 placeholder="Enter Transaction amount"
                                 name="amount"
                                 type="number"
+                                onChange={handleFormChange}
                             />
                         </FormControl>
-                        <RadioGroup mt={'5'}>
-                            <Radio mr={'5'} value="income" colorScheme="blue" name="type">Income</Radio>
-                            <Radio mr={'5'} value="expense" colorScheme="red" name="type">Expense</Radio>
+                        <RadioGroup mt={'5'} value={value} onChange={setValue}>
+                            <Radio 
+                            checked={formData.type === 'income'}
+                                mr={'5'} 
+                                value="income" 
+                                colorScheme="blue" 
+                                name="type"
+                                onChange={handleFormChange}
+                            >
+                                Income
+                            </Radio>
+                            <Radio 
+                                checked={formData.type === 'expense'}
+                                mr={'5'} 
+                                value="expense" 
+                                colorScheme="red" 
+                                name="type"
+                                onChange={handleFormChange}
+                            >
+                                Expense
+                            </Radio>
                         </RadioGroup>
                     </ModalBody>
                     <ModalFooter>
                         <Button mr={'4'} onClick={onClose}>Cancel</Button>
-                        <Button>Add</Button>
+                        <Button onClick={onClose} type="submit">Add</Button>
                     </ModalFooter>
                 </ModalContent>
             </form>
